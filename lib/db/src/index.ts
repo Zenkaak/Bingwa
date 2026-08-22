233fe4feeaa69c22e7a4ae8cc554c4326e9c3377
Zenkaak <jaykibo736@gmail.com>
2026-08-23 02:31:03 +0300

    Rebuild Bingwa with dense marketplace cards

--- lib/db/src/index.ts
+++ lib/db/src/index.ts
@@ -1,18 +1,16 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
-import * as schema from "./schema/index.js";
+import * as schema from "./schema";
 
 const { Pool } = pg;
 
-const databaseUrl = process.env.NEON_DATABASE_URL ?? process.env.DATABASE_URL;
-
-if (!databaseUrl) {
+if (!process.env.DATABASE_URL) {
   throw new Error(
-    "NEON_DATABASE_URL or DATABASE_URL must be set. Did you forget to provision a database?",
+    "DATABASE_URL must be set. Did you forget to provision a database?",
   );
 }
 
-export const pool = new Pool({ connectionString: databaseUrl });
+export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 export const db = drizzle(pool, { schema });
 
-export * from "./schema/index.js";
+export * from "./schema";
